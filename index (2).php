02a24d3770de6cd5511b129805ea3831aab28894
Hann1pp <164311912+Hann1pp@users.noreply.github.com>
2024-05-22 13:57:28 +0700

    Add files via upload

---
+++ index (2).php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title><style class="css"></style>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Welcome to the User Registration System</h2>
+        <a href="register.php">Register</a> | <a href="login.php">Login</a>
+    </div>
+</body>
+</html>
